c58bf4ce2e8beb55c8dec91a268df7e372ee63f3
howdopointerswork <dominusfaba@outlook.com>
2025-09-17 19:31:28 -0700

    Updated scripts

---
+++ run_xampp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+open /Applications/XAMPP/manager-osx.app
--- setup.sh
+++ setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ -d "project-3135" ]; then
+read -p "Enter Github Username: " NAME
+
+name=$NAME
+
+if [ -d "../project-3135" ]; then
 	echo "This Repo Has Already Been Cloned"
 else
 	echo "Cloning Repo..."
 	cd ~
 	sleep 2
-	git clone git@github.com:howdopointerswork/project-3135.git
+	git clone git@github.com:$name/project-3135.git
 	echo "Building Backup Folder..."
 	sleep 2
 	mkdir ~/recovery-3135
