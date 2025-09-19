6679eb89fd3b26e0f88a544c8d6e89a274b5324f
howdopointerswork <dominusfaba@outlook.com>
2025-09-18 19:37:10 -0700

    playing with js and php

--- htdocs/test.php
+++ htdocs/test.php
@@ -1,7 +1,7 @@
 <?php
 	$fname = isset($_GET["fname"]) ? $_GET["fname"] : "";
 	$lname = isset($_GET["lname"]) ? $_GET["lname"] : "";
-	
+	$val = isset($_GET["val"]) ? $_GET["val"] : "";		
 /*	if($fname != ""){
 		echo "Hello, " . htmlspecialchars($fname) . " " . htmlspecialchars($lname) . "!<br>";
 	}
@@ -19,6 +19,7 @@
 		<meta charset="utf-8">
 		<!-- css here -->
 		<link rel="stylesheet" href="test.css">
+		<script src="test.js"></script>
 	</head>
 	
 	<body>
@@ -33,8 +34,21 @@
 				<input type="submit" value="Submit">
 			</form>
 
-			<?php if($fname != ""){echo htmlspecialchars($fname);} ?>
+		<!--	<?php if($fname != ""){echo htmlspecialchars($fname);}?> -->
+
+			<form action="test.php" method="get">
+			<ul>
+				<li><button type="button" id="add" name="add">Add</button></li>
+				<li><input type="text" id="val" name="val"></li>
+				<li><button type="button" id="sub" name="sub">Sub</button></li>
+			</ul>
+
+			
+				<input type="submit" id="calc" name="calc" value="Calculate">
+			</form>
 		</div>
+
+<?php echo "The average is: " . $val?>
 	</body>
 
 </html>
