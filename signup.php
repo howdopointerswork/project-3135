cd736b636f84ff8e90b99a5bff1118b74fff7314
yant <149224933+yanki-bit@users.noreply.github.com>
2025-10-29 23:08:36 -0700

    Added mores structure files

--- signup.php
+++ signup.php
@@ -1,5 +1,14 @@
 <?php
-echo "Signup page";
+
+//variable for dynamic title
+$pageTitle = "Registration";
+//variable for dynamic header image
+$headerImg = "images/pexels-burak-the-weekender-735869.jpg";
+
+//for including the header from inc folder 
+include 'inc/header.php'; 
+
+?>
 
 ?>
 
