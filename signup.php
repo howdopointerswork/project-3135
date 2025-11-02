9935228d0dc61a17cce975cfa7300a8947615622
yant <149224933+yanki-bit@users.noreply.github.com>
2025-11-02 11:15:06 -0800

    Updated signup.php

--- signup.php
+++ signup.php
@@ -10,5 +10,17 @@ include 'inc/header.php';
 
 ?>
 
-?>
+    <h1>Sign Up</h1>
+    <form action="process_signup.php" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required />
+    
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+    
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+    
+        <button type="submit">Register</button>
+    </form>
 
