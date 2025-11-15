7c7d751b950b729013866be3a6535494fca5492b
yant <149224933+yanki-bit@users.noreply.github.com>
2025-11-15 10:59:20 -0800

    Updated footer styling and added css

---
+++ inc/footer.php
@@ -0,0 +1,58 @@
+<!-- Footer partial: include this from pages. -->
+<!-- Prefer to add the stylesheet in the site header; fallback link here. -->
+<link rel="stylesheet" href="/project-3135/css/footer.css">
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+<!-- Footer partial: include this from pages. -->
+
+<footer>
+  <div class="container">
+
+    <!-- Footer content -->
+      <div class="footer-content">
+        
+        <div class="upper-left">
+          <h3> About Us </h3>
+          <p> WyseCare is dedicated to providing the best healthcare services to our community. Our team of professionals is here to support your health and wellness journey. </p>
+        </div>
+
+        <div class="lower-left">
+          <h3>Contact Us</h3>
+          <div class="phone">
+            <a href="#"><i class="fas fa-phone"></i> +1 (555) 123-4567</a>
+          </div>
+          <div class="email">
+            <a href="mailto:freedolphin.haru@gmail.com" ><i class="fas fa-envelope"></i>wysecare@mail.ca
+          </div>
+        </div>
+
+      </div>
+
+      <div class="footer-content">
+        <h3>Quick Links</h3>
+        <ul>
+          <li><a href="/project-3135/main.php">Home</a></li>
+          <li><a href="/project-3135/about.php">About Us</a></li>
+          <li><a href="/project-3135/services.php">Services</a></li>
+          <li><a href="/project-3135/contact.php">Contact</a></li>
+        </ul>
+      </div>
+
+      <div class="footer-content">
+        <form action="/project-3135/search.php" method="get">
+          <h3>Newsletter Signup</h3>
+          <input type="email" name="email" placeholder="Enter your email" required>
+          <button type="submit" name="submit" value="Send">Subscribe</button>
+      </div>
+
+      <div class="bottom">
+           <p>Wyse Care 2025. All rights reserved. </p>
+      </div>
+
+
+  </div> <!-- end of class container -->
+   
+ 
+  </footer>
+</body>
+</html>
