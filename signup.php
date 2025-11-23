02571bf2f7ed8932e8810cf6ac41bdb29a5497e3
yant <149224933+yanki-bit@users.noreply.github.com>
2025-11-22 16:56:00 -0800

    Updated sign up to match the login page

--- signup.php
+++ signup.php
@@ -5,11 +5,12 @@ include 'inc/header.php';
 
 ?>
 
+<div class="login-gradient-wrapper">
 <?php
 
 $time = date('YYYY-mm-dd');
 
-echo "<h1 class='auth-title' id='signupTitle'>Sign Up</h1>";
+echo "<h1 class='auth-title login-title'>Create Your WyseCare Account</h1>";
 $fields = ['Username', 'Password', 'Age', 'Height', 'Weight', 'Gender'];
 
 echo "<form class='signup-form' method='post' action='main.php'>";
@@ -33,7 +34,10 @@ echo "<input class='signup-btn' type='submit' name='action' value='Add Account'>
 echo "</div>";
 echo "</form>";
 
+echo "<p style='text-align:center; color: #ffffff; margin-top: 1em;'>Already have an account? <a href='main.php?action=login' style='color: #ffd966; font-weight: 600;'>Log In</a></p>";
+
 ?>
+</div>
 
 <?php
 // Calls footer
